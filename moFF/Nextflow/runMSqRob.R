6a26e31ff13522ae45d2cd7e05dccc8b94b0c52a
veitveit <veitveit@gmail.com>
2020-09-07 10:53:28 +0200

    corrected non-working msqrob

--- moFF/Nextflow/runMSqRob.R
+++ moFF/Nextflow/runMSqRob.R
@@ -5,6 +5,11 @@ peptides <- MSqRob::import2MSnSet("moff.tab",filetype="moFF")
 #head(exprs(peptides))
 runs <- colnames(exprs(peptides))
 exp_annotation <- read.csv("exp_design.tsv",sep="\t")
+exp_annotation$genotype <- make.names(exp_annotation$genotype)
+for (c in 1:ncol(exp_annotation)) {
+  exp_annotation[,c] <- as.factor(exp_annotation[,c])
+}
+
 #print(exp_annotation)
 peptides <- preprocess_MSnSet(peptides,accession="prot",split=",", useful_properties="peptide", exp_annotation=exp_annotation)
 #head(fData(peptides))
@@ -12,11 +17,12 @@ peptides <- preprocess_MSnSet(peptides,accession="prot",split=",", useful_proper
 fData(peptides)[,"prot"] <- as.factor(fData(peptides)[,"prot"])
 # Set data.frame for experimental design with columns run genotype biorep
 proteins <- MSnSet2protdata(peptides, accession="prot")  
-#head(proteins)
-system.time(protLM <- fit.model(proteins, response="quant_value", fixed=c("genotype"),  random=c("biorep","run","peptide"), add.intercept=TRUE, weights="Huber"))
-# create comparisons vs first 
+      #head(proteins)
+system.time(protLM <- fit.model(proteins, response="quant_value", fixed=c("genotype"),  random=c("biorep","run","peptide"), add.intercept=TRUE))
+          #create comparisons vs first
 contrasts <- NULL
-levels <- unique(exp_annotation$genotype)
+levels <- as.factor(paste0("genotype",make.names(unique(exp_annotation$genotype))))
+# levels <- unique(exp_annotation$genotype)
 if (length(levels) > 1) {
   for (i in levels[-1]) {
     contrasts <- append(contrasts, paste(i, levels[1], sep="-"))  
@@ -24,7 +30,7 @@ if (length(levels) > 1) {
 } else {
   contrasts <- levels
 }
-L <- makeContrast(contrasts=contrasts,levels=levels)
-result <- test.protLMcontrast(protLM, L)
-result <- prot.p.adjust(result, method="fdr")
-write.csv(result, "MSqRobOut.csv")
+L <- makeContrast(contrasts=contrasts,levels=as.character(levels))
+    result <- test.protLMcontrast(protLM, L)
+  result <- prot.p.adjust(result, method="fdr")
+      write.csv(result, "MSqRobOut.csv")
